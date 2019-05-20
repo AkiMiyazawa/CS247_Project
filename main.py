bee5dd64ce61f98a34524afe285e2faaa360a2df
Derek Xu <derekqxu@gmail.com>
2019-05-20 13:08:36 -0700

    Update main.py
    Word Embeddings training works now :)

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ import torch.nn.functional as F
 
 class DataLoader:
     def __init__(self):
+        self.tokenized_X = None
+        self.tokenized_y = None
+        self.X = None
+        self.y = None
+        self.n_vocab = 0
+        self.vocab = {}
+        self.vocab_inv = {}
         pass        
 
     def load_data(self, verbose = True):
@@ -21,15 +28,15 @@ class DataLoader:
         
         self.tokenized_X = self._tokenize(dataset=toy_input)
         self.tokenized_y = self._tokenize(dataset=toy_output, keywords=True)
-        self._construct_vocabulary(tokenized_X, tokenized_y)
-        onehot_X = self._one_hot(dataset=tokenized_X)
-        onehot_y = self._one_hot(dataset=tokenized_y, keywords=True)
+        self._construct_vocabulary(self.tokenized_X, self.tokenized_y)
+        onehot_X = self._one_hot(dataset=self.tokenized_X)
+        onehot_y = self._one_hot(dataset=self.tokenized_y, keywords=True)
         self.X = onehot_X #torch.tensor(onehot_X)
         self.y = onehot_y #torch.tensor(onehot_y)
         if verbose:
             print('====================tokenized====================')
-            print(tokenized_X)
-            print(tokenized_y)
+            print(self.tokenized_X)
+            print(self.tokenized_y)
             '''
             print('====================onehot====================')
             print(onehot_X)
@@ -43,7 +50,7 @@ class DataLoader:
         return self.X, self.y
 
     def get_metadata(self):
-        return self.tokenized_X, self.tokenized_y, self.vocab, self.n_vocab, self.vocab_inv
+        return self.vocab, self.vocab_inv, self.n_vocab 
 
     def _tokenize(self, dataset=None, keywords=False):
         tokenized_dataset = []
@@ -100,69 +107,89 @@ class DataLoader:
             for word in sample:
                 if word not in self.vocab.keys():
                     self.vocab[word] = self.n_vocab
-					self.vocab_inv[self.n_vocab] = word
+                    self.vocab_inv[self.n_vocab] = word
                     self.n_vocab += 1
 
 # source: https://towardsdatascience.com/implementing-word2vec-in-pytorch-skip-gram-model-e6bae040d2fb
 # TODO: check if this implementation is correct
 class EmbeddingLayer:
-	def __init__(self, n_vocab, embedding_dim, window_size):
-		self.n_vocab = n_vocab
-		self.embedding_dim = embedding_dim
-		self.window_size = window_size
-	
-	def gen_embeddings(self, dataset, epochs, lr):
-		self._init_embeddings()
-		self._gen_idx_pairs(dataset)
-		self._train_embeddings(idx_pairs, epochs, lr)
-	
-	def get_embeddings(self, dataset):
-		# TODO: concatenate the pytorch vectors here!
-		pass
-	
-	def _gen_idx_pairs(self, dataset):
-		for sentence in dataset:
-			indices = [np.where(word == 1) for word in sentence]
-			for center_word_pos in range(len(indices)):
-				for w in range(-self.window_size, self.window_size + 1):
-					context_word_pos = center_word_pos + w
-					if context_word_pos < 0 or context_word_pos >= len(indices) or center_word_pos == context_word_pos:
-						continue
-					context_word_idx = indices[context_word_pos]
-					idx_pairs.append((indices[center_word_pos], context_word_idx))
-		self.idx_pairs = np.array(idx_pairs)
-	
-	def _init_embeddings(self):
-		self.W1 = Variable(torch.randn(self.embedding_dim, self.n_vocab).float(), requires_grad=True)
-		self.W2 = Variable(torch.randn(self.n_vocab, self.embedding_dim).float(), requires_grad=True)
-	
-	def _train_embeddings(self, epochs, lr):
-		for epoch in range(epochs):
-			loss_val = 0
-			for data, target in self.idx_pairs:
-				x = torch.zeros(n_vocab).float()
-				x[data] = 1.0
-				y_true = Variable(torch.from_numpy(np.array([target])).long())
-				z1 = torch.matmul(W1, x)
-				z2 = torch.matmul(W2, z1)
-				log_softmax = F.log_softmax(z2, dim=0)
-				loss = F.nll_loss(log_softmax.view(1,-1), y_true)
-				loss_val += loss.data.item()
-				loss.backward()
-				W1.data -= learning_rate * W1.grad.data
-				W2.data -= learning_rate * W2.grad.data
-			if epoch % 10 == 0:    
-				print('Loss at epoch {}: {}'.format(epoch,loss_val/len(self.idx_pairs)))
-	
+    def __init__(self, n_vocab, embedding_dim, window_size):
+        self.n_vocab = n_vocab
+        self.embedding_dim = embedding_dim
+        self.window_size = window_size
+    
+    def gen_embeddings(self, dataset, epochs, lr):
+        self._init_embeddings()
+        self._gen_idx_pairs(dataset)
+        self._train_embeddings(epochs, lr)
+    
+    def get_embeddings(self, dataset):
+        # TODO: concatenate the pytorch vectors here!
+        pass
+    
+    def _gen_idx_pairs(self, dataset):
+        idx_pairs = []
+        for sentence in dataset:
+            indices = [np.where(word == 1) for word in sentence]
+            for center_word_pos in range(len(indices)):
+                for w in range(-self.window_size, self.window_size + 1):
+                    context_word_pos = center_word_pos + w
+                    if context_word_pos < 0 or context_word_pos >= len(indices) or center_word_pos == context_word_pos:
+                        continue
+                    context_word_idx = indices[context_word_pos]
+                    assert len(context_word_idx) == 1
+                    context_word_idx = context_word_idx[0][0]
+                    idx_pairs.append((indices[center_word_pos], context_word_idx))
+        self.idx_pairs = idx_pairs
+    
+    def _init_embeddings(self):
+        self.W1 = Variable(torch.randn(self.embedding_dim, self.n_vocab).float(), requires_grad=True)
+        self.W2 = Variable(torch.randn(self.n_vocab, self.embedding_dim).float(), requires_grad=True)
+    
+    def _train_embeddings(self, epochs, lr):
+        for epoch in range(epochs):
+            loss_val = 0
+            for data, target in self.idx_pairs:
+                x = torch.zeros(self.n_vocab).float()
+                x[data] = 1.0
+                y_true = Variable(torch.from_numpy(np.array([target])).long())
+                z1 = torch.matmul(self.W1, x)
+                z2 = torch.matmul(self.W2, z1)
+                log_softmax = F.log_softmax(z2, dim=0)
+                '''
+                print('data')
+                print(data)
+                print('target')
+                print(target)
+                print('y_true')
+                print(y_true)
+                print('log_softmax')
+                print(log_softmax)
+                '''
+                loss = F.nll_loss(log_softmax.view(1,-1), y_true)
+                loss_val += loss.data.item()
+                loss.backward()
+                self.W1.data -= lr * self.W1.grad.data
+                self.W2.data -= lr * self.W2.grad.data
+
+                self.W1.grad.data = self.W1.grad.data.zero_()
+                self.W2.grad.data = self.W2.grad.data.zero_()
+            if epoch % 10 == 0:    
+                print('Loss at epoch {}: {}'.format(epoch,loss_val/len(self.idx_pairs)))
+
 def main():
+    # hyper parameters
+    embedding_dim = 10
+    window_size = 3
+    epochs = 100
+    lr = 1e-3
+
     data_loader = DataLoader()
     data_loader.load_data()
     X, y = data_loader.get_data()    
-    vocab, n_vocab = data_loader.get_metadata()
-	embedder = EmbeddingLayer(n_vocab, embedding_dim, window_size)
-	embedder.gen_embeddings(dataset=X, epochs=101, lr=1e-3)
-    X_embedding = embedder.get_embeddings(X)
-    y_embedding = embedder.get_embeddings(y)
+    vocab, inv_vocab, n_vocab = data_loader.get_metadata()
+    embedder = EmbeddingLayer(n_vocab, embedding_dim, window_size)
+    embedder.gen_embeddings(dataset=X, epochs=epochs, lr=lr)
 
 if __name__ == '__main__':
     main()
